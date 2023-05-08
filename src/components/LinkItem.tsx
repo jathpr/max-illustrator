0c7848513b1fd4d63fd5222ce42b05323a26f270
yury-odinson <yury.odinson@gmail.com>
2023-05-08 03:22:53 +0300

    fix(menu): repair images for footer links menu

--- src/components/LinkItem.tsx
+++ src/components/LinkItem.tsx
@@ -1,19 +1,14 @@
-import { StaticImage } from "gatsby-plugin-image"
-import React from "react";
-
 type LinkProps = {
     linkPage: string,
-    source: string,
-    alt: string
+    children: any // which type for <StaticImage />?
 }
 
-// StaticImage использовать вместо image
-
-export const LinkItem = ({ linkPage, source, alt }: LinkProps) => {
+export const LinkItem = ({ linkPage, children }: LinkProps) => {
     return (
         // console.log(source)
         <a className="menu-link__item" href={linkPage}>
-            <StaticImage src={source} alt={alt} width={50} height={50} />
+            {children}
+            {/* <StaticImage src={source} alt={alt} width={50} height={50} /> */}
         </a>
     )
 }
