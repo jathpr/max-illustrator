7d5e38ccfe5fac1ebdb9e57a45dd82fdf9c04aa8
Yury Odintsov <yury.odinson@gmail.com>
2023-05-09 22:12:37 +0300

    feat(menu): added animation menu

--- src/components/LinkItem.tsx
+++ src/components/LinkItem.tsx
@@ -5,10 +5,8 @@ type LinkProps = {
 
 export const LinkItem = ({ linkPage, children }: LinkProps) => {
     return (
-        // console.log(source)
         <a className="menu-link__item" href={linkPage}>
             {children}
-            {/* <StaticImage src={source} alt={alt} width={50} height={50} /> */}
         </a>
     )
 }
